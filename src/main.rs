1a532937d4ebbcb6993bbd5488013617700e60d3
Nino Lalanne-Tisné <ninolt.pro@gmail.com>
2024-12-05 00:54:17 +0100

    feat: Calls to the method in main.rs for cargo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod day_01;
+mod day_02;
+mod day_03;
 
 fn main() {
-    println!("Day 1 : First star result");
-    day_01::first_star::main();
-    println!("Day 1 : Second star result");
-    day_01::second_star::main();
+    // println!("Day 1 : First star result");
+    // day_01::first_star::main();
+    // println!("Day 1 : Second star result");
+    // day_01::second_star::main();
+    // println!("Day 2 : First star result");
+    // day_02::first_star::main();
+    // println!("Day 2 : Second star result");
+    // day_02::second_star::main();
+    //println!("Day 3 : First star result");
+    //day_03::first_star::main();
+    println!("Day 3 : Second star result");
+    day_03::second_star::main();
 }
